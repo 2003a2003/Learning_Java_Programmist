aa8095115ea832e7af1135deeae6997c6eb9425f
2003a2003 <2003a2003@ukr.net>
2016-03-09 10:04:48 +0200

    new create depisitory

---
+++ Learning_Java_Programmist/src/lesson2/part_6/frame_6/Launcher.java
@@ -0,0 +1,8 @@
+package lesson2.part_6.frame_6;
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        ActionField af = new ActionField();
+        af.runTheGame();
+    }
+}
