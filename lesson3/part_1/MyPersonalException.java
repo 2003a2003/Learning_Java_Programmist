20d35b07d9c61a219bef90a01e3aa5ed27e582f1
2003a2003 <2003a2003@ukr.net>
2016-03-11 00:39:26 +0200

    2016-03-11 0-39

--- lesson3/part_1/MyPersonalException.java
+++ lesson3/part_1/MyPersonalException.java
@@ -1,14 +1,9 @@
 package lesson3.part_1;
 
 public class MyPersonalException extends RuntimeException {
-
     public MyPersonalException(String txt){
-        System.out.println("Inside modul - 3: " + (10/0));
-        throw new ArithmeticException(txt);
-    }
-
-    public void secondException() throws IllegalAccessException{
-
-
+        if(txt.length() < 15) {
+            throw new MyPersonalException("txt.length() < 15");
+        }
     }
 }
