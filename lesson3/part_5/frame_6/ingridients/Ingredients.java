418595c3c3b7e7ee0dc9fc997e601326f7c7c83a
2003a2003 <2003a2003@ukr.net>
2016-03-18 18:37:08 +0200

    2016-03-16 18-36

---
+++ lesson3/part_5/frame_6/ingridients/Ingredients.java
@@ -0,0 +1,8 @@
+package lesson3.part_5.frame_6.ingridients;
+
+public class Ingredients {
+
+    private String ingridientsName;
+    private int coll;
+    private int price;
+}
---
+++ lesson3/part_5/frame_6/ingridients/Water.java
@@ -0,0 +1,7 @@
+package lesson3.part_5.frame_6.ingridients;
+
+/**
+ * Created by otroshenko on 18.03.2016.
+ */
+public class Water {
+}
