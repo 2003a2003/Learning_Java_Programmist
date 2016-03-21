097eb4b6b9362cc59c730a25e087ee1f47e499e0
2003a2003 <2003a2003@ukr.net>
2016-03-21 11:08:26 +0200

    2016-03-21 11-00

--- lesson3/part_5/frame_6/ingridients/Chocolate.java
+++ lesson3/part_5/frame_6/ingridients/Chocolate.java
@@ -1,14 +1,5 @@
 package lesson3.part_5.frame_6.ingridients;
 
 public class Chocolate extends AbstractIngridients{
-//
-//    public Chocolate() {
-//        this(100);
-//    }
-//
-//    public Chocolate(int dose) {
-//        setNameOfIngredient("Chocolate");
-//        setPrice(2.2);
-//        setDose(dose);
-//    }
+
 }
