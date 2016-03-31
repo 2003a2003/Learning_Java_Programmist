0bf43ed165e906fd64628136766fc5441520a9ae
2003a2003 <2003a2003@ukr.net>
2016-03-31 16:10:01 +0300

    lesson_4.part_01.frame_01

---
+++ lesson4/part_02/frame_01/Node.java
@@ -0,0 +1,25 @@
+package lesson4.part_02.frame_01;
+
+public class Node {
+    private Object o;
+    private Node node;
+
+    public Node() {
+    }
+
+    public Object getO() {
+        return o;
+    }
+
+    public void setO(Object o) {
+        this.o = o;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
+}
---
+++ lesson4/part_02/frame_01/SimpleLinkedList.java
@@ -0,0 +1,25 @@
+package lesson4.part_02.frame_01;
+
+public class SimpleLinkedList {
+    private Node root;
+    private int size;
+
+    public SimpleLinkedList() {
+        size = 0;
+    }
+
+    public void addFirst(Object o){
+
+    }
+
+    public void addLast(Object o){
+
+    }
+    public void addAfter(Object o1, Object o2){
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
