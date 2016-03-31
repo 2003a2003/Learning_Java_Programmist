d6048831851757b78ae54e1647a585c7c8b5ef25
2003a2003 <2003a2003@ukr.net>
2016-03-31 16:22:22 +0300

    create class Node in the class SimpleLinkedList

---
+++ lesson4/part_02/frame_03/Demo.java
@@ -0,0 +1,9 @@
+package lesson4.part_02.frame_03;
+
+public class Demo {
+    public static void main(String[] args) {
+        SimpleLinkedList s = new SimpleLinkedList();
+        System.out.println("Size: " + s.getSize());
+
+    }
+}
---
+++ lesson4/part_02/frame_03/SimpleLinkedList.java
@@ -0,0 +1,33 @@
+package lesson4.part_02.frame_03;
+
+public class SimpleLinkedList {
+    private Node root;
+    private int size;
+
+    public SimpleLinkedList() {
+        root = new Node();
+        size = 0;
+
+    }
+
+    public void addFirst(Object o){
+
+    }
+
+    public void addLast(Object o){
+
+    }
+    public void addAfter(Object o1, Object o2){
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private class Node {
+        private Object o;
+        private Node node;
+    }
+
+}
