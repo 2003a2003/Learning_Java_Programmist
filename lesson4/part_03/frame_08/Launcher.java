84a22132c0f4731cb7ce7b0dde52e48a39d88f7b
2003a2003 <2003a2003@ukr.net>
2016-04-04 16:33:42 +0300

    lesson4.part_03.frame_08

---
+++ lesson4/part_03/frame_08/Launcher.java
@@ -0,0 +1,39 @@
+package lesson4.part_03.frame_08;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Person per = new Person();
+        per.setName("Alex");
+        per.setAge(25);
+        per.setSalary(15500);
+
+        Address adr1 = new Address();
+        adr1.setCity("Kiev");
+        adr1.setStreet("Pushkina");
+        adr1.setHouse(5);
+
+        per.setAddress(adr1);
+
+        Person per2 = new Person();
+        per2.setName("Alex");
+        per2.setAge(25);
+        per2.setSalary(15500);
+//        per2.setAddress(adr1);
+
+        Person per3 = new Person();
+        //per3.setName("Boris");
+        per3.setAge(30);
+        per3.setSalary(3800);
+
+        System.out.println(per.getName() + " " + per.hashCode());
+        System.out.println(per2.getName() + " " + per2.hashCode());
+        System.out.println(per3.getName() + " " + per3.hashCode());
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("equals: " + per.equals(per2));
+        System.out.println("equals: " + per.equals(per3));
+
+    }
+}
---
+++ lesson4/part_03/frame_08/Person.java
@@ -0,0 +1,86 @@
+package lesson4.part_03.frame_08;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private long salary;
+    private Address address;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public int hashCode() {
+        final int perem = 50;
+        int rez = 5;
+
+        if (name == null) {
+            rez = perem * rez + ((Integer) age).hashCode();
+            rez = perem * rez + ((Long) salary).hashCode();
+        } else {
+            rez = perem * rez + name.hashCode();
+            rez = perem * rez + ((Integer) age).hashCode();
+            rez = perem * rez + ((Long) salary).hashCode();
+        }
+
+        if(address != null){
+            rez = perem * rez + address.hashCode();
+        }
+
+        /*
+        primer Olega
+        int result = 25;
+        result = 37 * result + name.hashCode();
+        result = 37 * result + age;
+        result = 37 * result + new Long(salary).hashCode();
+        //faster, copy implementation
+        //result = 37 * result + ((int)(salary^(salary >>> 32)));
+        */
+
+        return rez;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Person)) {
+            return false;
+        }
+        Person pers = (Person) obj;
+
+        return (name != null && name.equals(pers.getName()) && (age == pers.getAge() && salary == pers.getSalary())
+        && address.equals(pers.address));
+    }
+}
