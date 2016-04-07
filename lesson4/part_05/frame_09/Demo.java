8d42aba71dfbc961b820000a1d2c33f0391ba519
2003a2003 <2003a2003@ukr.net>
2016-04-07 17:48:45 +0300

    lesson4.part_05.frame_09 create

---
+++ lesson4/part_05/frame_09/Demo.java
@@ -0,0 +1,4 @@
+package lesson4.part_05.frame_09;
+
+public class Demo {
+}
---
+++ lesson4/part_05/frame_09/Stack.java
@@ -0,0 +1,24 @@
+package lesson4.part_05.frame_09;
+
+public class Stack {
+    private Object object;
+
+
+    public Stack() {
+
+    }
+
+    public void push(Object object){
+
+    }
+
+    public Object pop(){
+
+        return new Object();
+    }
+
+    public Object peak(){
+
+        return new Object();
+    }
+}
