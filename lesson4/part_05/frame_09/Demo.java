4fa62e6ada571e3fd5135b5618cb5112ae4f9ff3
2003a2003 <2003a2003@ukr.net>
2016-04-11 11:04:32 +0300

    lesson4.part_05.frame_09 create

--- lesson4/part_05/frame_09/Demo.java
+++ lesson4/part_05/frame_09/Demo.java
@@ -1,4 +1,29 @@
 package lesson4.part_05.frame_09;
 
 public class Demo {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        //Add object to stack
+        stack.push("One");
+        stack.push("Two");
+        stack.push("Three");
+        stack.push("Four");
+        stack.push("Five");
+        stack.push("Six");
+        stack.push("Seven");
+        stack.push("Eight");
+        stack.push("Nine");
+
+        //Print stack
+        stack.printList();
+
+        //remove first object
+        stack.pop();
+        System.out.println();
+        stack.printList();
+        System.out.println();
+
+        //get first object
+        System.out.println(stack.peak());
+    }
 }
--- lesson4/part_05/frame_09/Stack.java
+++ lesson4/part_05/frame_09/Stack.java
@@ -1,24 +1,40 @@
 package lesson4.part_05.frame_09;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
 public class Stack {
     private Object object;
-
+    private LinkedList<Object> l = new LinkedList();
 
     public Stack() {
-
     }
 
-    public void push(Object object){
-
+    public void push(Object object) {
+        l.addLast(object);
     }
 
-    public Object pop(){
-
-        return new Object();
+    public Object pop() {
+        if (l.size() != 0) {
+            Object object = l.getFirst();
+            l.removeFirst();
+            return object;
+        } else {
+            throw new NoSuchElementException("An empty array");
+        }
     }
 
-    public Object peak(){
+    public Object peak() {
+        if (l.size() != 0) {
+            return l.getFirst();
+        } else {
+            throw new NoSuchElementException("An empty array");
+        }
+    }
 
-        return new Object();
+    public void printList() {
+        for (Object o : l) {
+            System.out.println(o.toString());
+        }
     }
 }
