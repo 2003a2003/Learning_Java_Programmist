0f03405f6f17674462b35d6059824bcfa856503e
2003a2003 <2003a2003@ukr.net>
2016-04-22 17:07:19 +0300

    chenge

--- lesson5/part_01/frame_08/FirstWindows.java
+++ lesson5/part_01/frame_08/FirstWindows.java
@@ -32,9 +32,8 @@ public class FirstWindows extends JPanel {
     public static void main(String[] args) throws Exception {
         FirstWindows f = new FirstWindows();
 
-        Thread.sleep(1500);
-        System.out.println("woke up");
         while (x < 300) {
+            Thread.sleep(1000);
             x += 50;
             y += 50;
             f.repaint();
