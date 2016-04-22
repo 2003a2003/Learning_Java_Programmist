cd5a44484712453b5830bd215bfc3e3ded5bd96d
2003a2003 <2003a2003@ukr.net>
2016-04-22 16:16:47 +0300

    lesson5.part_01.frame_04 create

---
+++ lesson5/part_01/frame_04/FirstWindows.java
@@ -0,0 +1,12 @@
+package lesson5.part_01.frame_04;
+
+import javax.swing.*;
+
+public class FirstWindows {
+    public static void main(String[] args) {
+        JFrame win = new JFrame();
+        win.setVisible(true);
+        win.setName("My First windows!!!");
+        win.setBounds(300,100,800,600);
+    }
+}
