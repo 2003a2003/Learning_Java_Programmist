506a7c453b72321e6a02e229531b065e1347f93c
2003a2003 <2003a2003@ukr.net>
2016-04-22 16:24:32 +0300

    after watching the video

--- lesson5/part_01/frame_04/FirstWindows.java
+++ lesson5/part_01/frame_04/FirstWindows.java
@@ -1,12 +1,23 @@
 package lesson5.part_01.frame_04;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FirstWindows {
     public static void main(String[] args) {
         JFrame win = new JFrame();
+
+        //My Program
+//        win.setName("My First windows!!!");
+//        win.setBounds(300,100,800,600);
+//        win.setVisible(true);
+
+        //Uchitel variant
+        win.setMinimumSize(new Dimension(800, 600));
+        win.setLocation(300, 100);
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.pack();
         win.setVisible(true);
-        win.setName("My First windows!!!");
-        win.setBounds(300,100,800,600);
+
     }
 }
