33c3ace74819aa6aa8a47eaf6d43984cd295a10e
2003a2003 <2003a2003@ukr.net>
2016-06-01 15:37:51 +0300

    lesson5.part_05.frame_03 create

--- lesson6/part_01/v2/calc/Adapter.java
+++ lesson6/part_01/v2/calc/Adapter.java
@@ -1,16 +1,7 @@
 package lesson6.part_01.v2.calc;
 
-import lesson6.part_01.Arifmetika;
-
-public class Adapter extends Arifmetika implements Calculator{
-
-    @Override
-    public int summa(int a, int b) {
-        return super.summa(new int[] {a, b});
-    }
-
-    @Override
-    public int multiply(int a, int b) {
-        return super.multiply(a,b);
-    }
+/**
+ * Created by otroshenko on 01.06.2016.
+ */
+public class Adapter {
 }
