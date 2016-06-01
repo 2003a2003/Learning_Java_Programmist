ccd42c8faa2935d1347a527fbd534a98f673dc95
2003a2003 <2003a2003@ukr.net>
2016-06-01 14:56:04 +0300

    +

--- lesson6/part_01/Launcher.java
+++ lesson6/part_01/Launcher.java
@@ -1,17 +1,19 @@
 package lesson6.part_01;
 
-import com.midgardabc.day9.adapter.math.v1.Operations;
-import com.midgardabc.day9.adapter.math.v1.calc.Calculator;
+import lesson6.part_01.v2.Operations;
+import lesson6.part_01.v2.calc.Adapter;
+import lesson6.part_01.v2.calc.Calculator;
 
 import java.util.Random;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		Calculator calc = new Calculator();
+		Calculator calc = new Adapter();
 		
-		com.midgardabc.day9.adapter.math.v1.Operations o = new Operations();
+		Operations o = new Operations();
 		o.setCalc(calc);
+
 
 		Random r = new Random();
 		for (int i = 0; i < 5; i++) {
