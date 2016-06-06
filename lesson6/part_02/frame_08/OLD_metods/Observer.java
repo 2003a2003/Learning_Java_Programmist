8cea76b7157058c87b6c427f4c29d283c0fea2d2
2003a2003 <2003a2003@ukr.net>
2016-06-06 19:24:47 +0300

    ready

--- lesson6/part_02/frame_08/OLD_metods/Observer.java
+++
@@ -1,5 +0,0 @@
-package lesson6.part_02.frame_08.OLD_metods;
-
-public interface Observer {
-    public void update();
-}
--- lesson6/part_02/frame_08/edition.java
+++
@@ -1,5 +0,0 @@
-package lesson6.part_02.frame_08;
-
-public enum Edition {
-    NEWSPAPER,MAGAZINE;
-}
