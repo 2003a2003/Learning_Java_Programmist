9d6487b43b51f32cf355617948d413328cdf7b18
2003a2003 <2003a2003@ukr.net>
2016-07-27 23:47:51 +0300

    +

--- lesson8/part_05/frame_04/demo/Demo.java
+++ lesson8/part_05/frame_04/demo/Demo.java
@@ -1,19 +1,21 @@
 package lesson8.part_05.frame_04.demo;
 
+import java.util.Arrays;
+
 public class Demo {
     public static void main(String[] args) {
-        Replace r = new Replace();
-        r.write("Test1");
-        r.write("Test2");
-        r.write("Test3");
-        r.write("Test4");
-        r.write("Test5");
-
-        //r.replace("Test1", "Test12");
-
-        r.print();
-
-        r.remove("Test2");
+//        Replace r = new Replace();
+//        r.write("Test1");
+//        r.write("Test2");
+//        r.write("Test3");
+//        r.write("Test4");
+//        r.write("Test5");
+//
+//        //r.replace("Test1", "Test12");
+//
+//        r.print();
+//
+//        r.remove("Test2");
 
     }
 }
