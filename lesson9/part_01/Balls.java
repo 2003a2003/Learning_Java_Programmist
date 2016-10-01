1551a14f5a0eada74a636d82a8cc116c059477ad
2003a2003 <2003a2003@ukr.net>
2016-10-01 14:02:36 +0300

    gotovo $) rfr vishlo - gal chto tak pozdno

--- lesson9/part_01/Balls.java
+++ lesson9/part_01/Balls.java
@@ -1,71 +1,120 @@
 package lesson9.part_01;
 
-import lesson1.part_4.frame_4.Color;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
 
-public class Balls extends JPanel{
+public class Balls extends JPanel {
 
-    private int speed;
-    private Color color;
-    private int x;
-    private int y;
     private int WIDTH = 300;
     private int HEIGHT = 500;
 
-    private static final Color[] COLORS = new Color[]{
-            Color.RED,
-            Color.ORANGE,
-            Color.AMARANTH,
-            Color.BLACK,
-            Color.BLUE,
-            Color.BROWN,
-            Color.CERISE,
-            Color.CORAL_RED,
-            Color.DARK_PINK,
-            Color.DARK_RED
-    };
+    private Ball ball;
+
+    private ArrayList<Ball> someBolls = new ArrayList<>();
+    private Color[] color;
 
     public Balls() throws InterruptedException {
+
         JFrame f = new JFrame("BALL");
-        f.setLocation(300,300);
+        f.setLocation(300, 300);
         f.setMinimumSize(new Dimension(HEIGHT, WIDTH));
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.getContentPane().add(this);
         f.pack();
         f.setVisible(true);
 
-//        createBalls();
-            action();
+        crateColorsArray();
 
+        createBall();
     }
 
+    private void crateColorsArray() {
+        color = new Color[]{
+                    Color.BLACK,
+                    Color.DARK_GRAY,
+                    Color.BLUE,
+                    Color.GREEN,
+                    Color.CYAN,
+                    Color.PINK,
+                    Color.pink,
+                    Color.GRAY,
+                    Color.YELLOW,
+                    Color.ORANGE
+        };
+    }
+
+    private void createBall() throws InterruptedException {
+        int i = 0;
+        int x = 0;
+        int y = 0;
+        int speed = 1;
+
+        while (i < color.length) {
+            ball = new Ball(x, y, speed, color[i]);
+            someBolls.add(ball);
+            ball.start();
+            i++;
+            y += 30;
+            speed += 2;
+        }
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(java.awt.Color.BLUE);
-        g.fillOval(x,y,20,20);
-    }
-
-    public void action() throws InterruptedException {
-        boolean todo = true;
-        while (true){
-            if(todo == true){
-                x++;
-                if (x > HEIGHT-40){
-                    todo = false;
-                }
-            } else {
-                x--;
-                if (x < 0) {
-                    todo = true;
-                }
-            }
-            Thread.sleep(10);
-            repaint();
+        for (Ball b : someBolls) {
+            b.draw(g);
         }
     }
 
+    class Ball extends Thread {
+
+        private int x;
+        private int y;
+        private int speed;
+        private Color color;
+
+        public Ball(int x, int y, int speed, Color color) {
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+            this.color = color;
+        }
+
+        public void draw(Graphics g) {
+            g.setColor(color);
+            g.fillOval(x, y, 20, 20);
+            repaint();
+        }
+
+        public void action() throws InterruptedException {
+            boolean todo = true;
+            while (true) {
+                if (todo == true) {
+                    x++;
+                    if (x > HEIGHT - 40) {
+                        todo = false;
+                    }
+                } else {
+                    x--;
+                    if (x < 0) {
+                        todo = true;
+                    }
+                }
+                Thread.sleep(speed);
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                action();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
